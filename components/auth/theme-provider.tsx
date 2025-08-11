0554b9fa736a120f0fc2f45fbcf2fbc3efe63692
Rizwan <mrizwanali6@gmail.com>
2025-08-11 13:57:21 +0500

    Remove unused components and add Netlify deployment configuration

--- components/auth/theme-provider.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-
-// Theme provider is no longer needed since we removed theme functionality
-export function ThemeProvider({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
-}
